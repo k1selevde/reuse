ba66b9bea94865930c0cf8b6f06a337b21cac7b7
Denis <k1selevde@yandex.ru>
2020-10-16 23:33:57 +0500

    add frontend

---
+++ graphql/graphql-start/application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -0,0 +1,29 @@
+import { withStyles } from '@material-ui/core/styles';
+import { compose } from 'recompose';
+import { graphql } from 'react-apollo';
+
+import { addDirectorMutation, updatedDirectorMutation } from './mutations';
+import { directorsQuery } from '../DirectorsTable/queries';
+
+import { styles } from './styles';
+
+const withGraphQL = compose(
+    graphql(addDirectorMutation, {
+        props: ({ mutate }) => ({
+            addDirector: director => mutate({
+                variables: director,
+                refetchQueries: [{ query: directorsQuery }],
+            }),
+        }),
+    }),
+    graphql(updatedDirectorMutation, {
+        props: ({ mutate }) => ({
+            updateDirector: director => mutate({
+                variables: director,
+                refetchQueries: [{ query: directorsQuery }],
+            }),
+        }),
+    })
+);
+
+export default compose(withStyles(styles), withGraphQL);
