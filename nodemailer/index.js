64862d768bc27d06af0697f5e595e344d89b1cb2
Denis <k1selevde@yandex.ru>
2021-01-05 23:22:50 +0500

    routes

--- nodemailer/index.js
+++ nodemailer/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 
 const app = express();
 
 const PORT = 3001;
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let user = undefined;
+
+app.post('/registration', (req,res) => {
+    if (!req.body.email || !req.body.password) return res.sendStatus(400)
+    console.log(req.body)
+    user = req.body
+    res.send('Регистрация успешна')
+    res.redirect('/registration')
+})
+
 app.get('/registration', (req,res) => {
-    res.sendFile(__dirname + '/registration.html')
+    if (typeof user !== 'object') return res.sendFile(__dirname + '/registration.html')
+    res.send('Регистрация успешна')
+    user = undefined
 })
 
 app.listen(PORT, () => console.log(`Server has been started on ${PORT} port`))
